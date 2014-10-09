52e534118064ce4560184b73f1eeceb7ffce696a
mrgoonie <goonnguyen@Goons-MacBook-Pro.local>
2014-10-09 10:35:08 +0700

    Initial Commit

---
+++ GTween/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GTween
+//
+//  Created by Goon Nguyen on 10/9/14.
+//  Copyright (c) 2014 Goon Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
